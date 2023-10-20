67e1081e655f1512bc70fa4d840f4f6939da1007
LUCAS SAPUCAHY GALVANO LINS <lucassapucahy@hotmail.com>
2023-10-20 12:26:51 -0300

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://portfolio-server-i35f5if98-lucassapucahy.vercel.app/projetos'
+  apiUrl: 'https://portfolio-server-lucassapucahy.vercel.app/projetos'
 };
